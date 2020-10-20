1cc38d66accec345c06b32ac6a6ca8312d6185ff
alenvoina <70748371+alenvoina@users.noreply.github.com>
2020-10-20 18:17:37 +0300

    Complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let check =  bracketsConfig[i].join('')
+ 
+    if (str.includes(check)) {
+      str = str.replace(check, '')
+      i =- 1
+    }
+  }
+  return str ? false : true
+
 }
